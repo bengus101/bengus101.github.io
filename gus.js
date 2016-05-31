307725540476f0f29dcaff4e8c4bf6bcd34564a5
bengus101 <bgustafson29@yahoo.com>
2016-05-31 16:50:43 -0700

    Added button for resume section. Need to wire up modal next.

---
+++ gus.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+      $('.carousel').carousel();
+
+      $('.carousel').carousel('next', [1]);
+
+      $('.carousel').carousel('prev', [1]);
+
+      // $('carousel').carousel('padding', (1000);
+
+      // $('carousel').carousel-item('dist', 200);
+    });
+
+// $(document).ready(function(){
+//     $('#scroll-it').sectionScroll();
+//     $('.modal-trigger').leanModal();
+//     $('.modal-trigger2').leanModal();
+//   });

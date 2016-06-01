dfaecb6f4fcc65bd79610403b579db7d6fd08d0a
bengus101 <bgustafson29@yahoo.com>
2016-05-31 23:16:42 -0700

    Modal now works. Resume needs to be formatted.

--- gus.js
+++ gus.js
@@ -4,10 +4,6 @@ $(document).ready(function(){
       $('.carousel').carousel('next', [1]);
 
       $('.carousel').carousel('prev', [1]);
-
-      // $('carousel').carousel('padding', (1000);
-
-      // $('carousel').carousel-item('dist', 200);
     });
 
 // $(document).ready(function(){
